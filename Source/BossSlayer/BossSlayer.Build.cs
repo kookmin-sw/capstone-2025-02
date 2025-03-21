e022b553a050e2906bae82a8781faf10c2b4a455
706-Camille <rlthro2@gmail.com>
2025-03-21 16:30:33 +0900

    Update build cs

--- Source/BossSlayer/BossSlayer.Build.cs
+++ Source/BossSlayer/BossSlayer.Build.cs
@@ -8,7 +8,7 @@ public class BossSlayer : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayCameras", "UMG"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayCameras", "UMG", "AIModule", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
